b1c33c6d5d01c98e7c898a26a13e4c4d69ddb68c
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2024-06-17 23:46:27 +0100

    update

--- tangerine-uniforms/src/component/body.jsx
+++ tangerine-uniforms/src/component/body.jsx
@@ -76,10 +76,10 @@ const Body = () => {
           <h1 className='text-gray-950 font-bold'>Reviews from our clients</h1>
           <p>See what our clients have to say about our services:</p>
           <div className='pt-8'>
-            <div className='flex flex-row place-content-center'>
-              <img src={Rev5} alt="" className='sm:h-32 sm:w-24 h-80 w-64'/>
-              <img src={Rev6} alt="" className='sm:h-32 sm:w-24 h-80 w-64'/>
-              <img src={Rev8} alt="" className='sm:h-32 sm:w-24 h-80 w-64'/>
+            <div className='sm:flex-col flex flex-row place-content-center'>
+              <img src={Rev5} alt="" className='h-80 w-64'/>
+              <img src={Rev6} alt="" className='h-80 w-64'/>
+              <img src={Rev8} alt="" className='h-80 w-64'/>
             </div>
             <div></div>
           </div>
